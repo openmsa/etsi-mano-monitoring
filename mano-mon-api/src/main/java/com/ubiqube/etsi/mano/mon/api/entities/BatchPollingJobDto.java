8b48a5ca855c632ec0bf4d77582cbc8b002379dd
Olivier Vignaud <ovi@ubiqube.com>
2025-02-18 17:16:33 +0100

    📄 api: Add missing license header.

--- mano-mon-api/src/main/java/com/ubiqube/etsi/mano/mon/api/entities/BatchPollingJobDto.java
+++ mano-mon-api/src/main/java/com/ubiqube/etsi/mano/mon/api/entities/BatchPollingJobDto.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2019-2025 Ubiqube.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.ubiqube.etsi.mano.mon.api.entities;
 
 import java.time.ZonedDateTime;
@@ -11,8 +27,10 @@ import com.ubiqube.etsi.mano.dao.mano.InterfaceInfo;
 import com.ubiqube.etsi.mano.service.mon.dto.MonConnInformationDto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class BatchPollingJobDto<I extends InterfaceInfo, A extends AccessInfo> {
 	/**
 	 * Internal ID.
