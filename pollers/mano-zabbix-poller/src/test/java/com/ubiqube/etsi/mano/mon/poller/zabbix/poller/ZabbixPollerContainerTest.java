58f84c5e43d173f890112b0e349c1bc8afc13adf
Olivier Vignaud <ovi@ubiqube.com>
2025-12-11 14:36:25 +0100

    🐛 serializer-hppa: Fix import on assertTrue.

--- pollers/mano-zabbix-poller/src/test/java/com/ubiqube/etsi/mano/mon/poller/zabbix/poller/ZabbixPollerContainerTest.java
+++ pollers/mano-zabbix-poller/src/test/java/com/ubiqube/etsi/mano/mon/poller/zabbix/poller/ZabbixPollerContainerTest.java
@@ -19,6 +19,7 @@ package com.ubiqube.etsi.mano.mon.poller.zabbix.poller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
